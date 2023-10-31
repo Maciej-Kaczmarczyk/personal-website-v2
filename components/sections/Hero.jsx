bbe93c43cf4ec8e41f4c21b3383b6199920b6393
Maciej Kaczmarczyk <maciej.kaczmarczyk01@gmail.com>
2023-10-31 22:56:48 +0100

    add favicon

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
       id="hero"
       className="relative z-30 mb-32 xL:mb-64 flex h-fit w-[100vw] justify-center pt-16 sm:pt-32 xl:pt-72 xl:mb-96"
     >
-      <div className="relative flex h-full flex-col items-center gap-7 text-center">
+      <div className="relative flex h-full flex-col items-center gap-7 text-center z-30">
         <h2 className="relative z-20 bg-gradient-to-b from-[#E8EBF1] to-[#B7B7B7] bg-clip-text px-4 py-2 text-5xl font-extrabold tracking-tight text-transparent drop-shadow-sm lg:text-8xl">
           <span className="bg-gradient-to-b from-[#E8EBF1] to-[#B7B7B7] bg-clip-text text-transparent">
             Need a{" "}
@@ -22,14 +22,14 @@ const Hero = () => {
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quam cum
           praesentium ad harum ullam placeat
         </h1>
-        <div className="mt-4 flex w-[100vw]  justify-center gap-8 px-4 lg:w-fit">
+        <div className="mt-4 flex w-[100vw] justify-center gap-8 px-4 z-30 lg:w-fit">
           <ButtonFill className="w-1/2 lg:w-[200px]">Contact</ButtonFill>
           <ButtonOutline className="w-1/2 lg:w-[200px]">
             Portfolio
           </ButtonOutline>
         </div>
-        <CurvedLine className="absolute -top-[300px] z-10 lg:-top-[230px] " />
-        <CurvedLine2 className="absolute -top-[160px] z-10 lg:-top-[70px]" />
+        <CurvedLine className="absolute -top-[300px] z-0 lg:-top-[230px] " />
+        <CurvedLine2 className="absolute -top-[160px] z-0 lg:-top-[70px]" />
       </div>
 
       <div className="absolute top-40 left-0 -z-10 aspect-square w-[30vw] rounded-full bg-blue blur-[70px] dark:opacity-70 sm:blur-[100px] lg:blur-[200px]"></div>
